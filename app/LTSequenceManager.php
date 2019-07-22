99016d36040ed28a25d8d8f7a324bde4a9af5361
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-23 03:02:03 +0900

    Refine code

--- app/LTSequenceManager.php
+++ app/LTSequenceManager.php
@@ -16,10 +16,39 @@ class LTSequenceManager
      */
     public function __construct($argv)
     {
-        if ($this->isFilePath($argv)) $names = $this->readCsvFile($argv);
-        else                          $names = $this->readStrings($argv);
+        if (Utils::isFilePath($argv)) $this->readCsvFile($argv);
+        else                          $this->readStrings($argv);
+        $this->shuffle();
+    }
+
+    /**
+     * CSVファイルを読み込む
+     *
+     * @param $filePath
+     */
+    private function readCsvFile($filePath)
+    {
+        $names = Utils::csvToArray($filePath);
         $names = Utils::arrayCollapse($names);
-        $this->names = $this->shuffle($names);
+        $this->names = $names;
+    }
+
+    /**
+     * 文字列を読み込む
+     *
+     * @param $argv
+     */
+    private function readStrings($argv)
+    {
+        $this->names = Utils::stringToArray($argv);
+    }
+
+    /**
+     * シャッフル
+     */
+    private function shuffle()
+    {
+        $this->names = Utils::fisherYatesShuffle($this->names);
     }
 
     /**
@@ -30,68 +59,5 @@ class LTSequenceManager
         foreach ($this->names as $index => $name) {
             echo "\033[0;32m" . ($index + 1) . ": $name\033[0m\n";
         }
-    }
-
-    /**
-     * ファイルパスかどうか
-     *
-     * @param $argv
-     * @return bool
-     */
-    private function isFilePath($argv)
-    {
-        if (strpos($argv, '/') === false) return false;
-        return true;
-    }
-
-    /**
-     * CSVファイルを読み込んで配列に変換する
-     *
-     * @param $filePath
-     * @return array
-     */
-    private function readCsvFile($filePath)
-    {
-        $file = new \SplFileObject($filePath);
-        $file->setFlags(\SplFileObject::READ_CSV);
-
-        $records = [];
-        foreach ($file as $line) {
-            mb_convert_variables('UTF-8', ['ASCII', 'JIS', 'EUC-JP', 'SJIS'], $line[0]);
-            $records[] = $line;
-        }
-
-        return $records;
-    }
-
-    /**
-     * 文字列を配列に変換する
-     *
-     * @param $argv
-     * @return array
-     */
-    private function readStrings($argv)
-    {
-        $names = str_replace(' ', '', $argv);
-        $names = explode(',', $names);
-        return $names;
-    }
-
-    /**
-     * Fisher-Yates Shuffle
-     *
-     * @param $array
-     * @return mixed
-     */
-    private function shuffle($array)
-    {
-        for ($i = count($array) - 1; $i > 0; $i--) {
-            $r = mt_rand(0, $i);
-            $tmp = $array[$i];
-            $array[$i] = $array[$r];
-            $array[$r] = $tmp;
-        }
-
-        return $array;
     }
 }
--- app/Utils.php
+++ app/Utils.php
@@ -27,4 +27,69 @@ class Utils
         }
         return $result;
     }
+
+    /**
+     * Fisher-Yates Shuffle
+     *
+     * @param $array
+     * @return mixed
+     */
+    public static function fisherYatesShuffle($array)
+    {
+        for ($i = count($array) - 1; $i > 0; $i--) {
+            $r = mt_rand(0, $i);
+            $tmp = $array[$i];
+            $array[$i] = $array[$r];
+            $array[$r] = $tmp;
+        }
+
+        return $array;
+    }
+
+    /**
+     * ファイルパスかどうか
+     *
+     * @param $argv
+     * @return bool
+     */
+    public static function isFilePath($argv)
+    {
+        if (strpos($argv, '/') === false) return false;
+        return true;
+    }
+
+    /**
+     * CSVファイルを配列に変換する
+     *
+     * @param $filePath
+     * @return array
+     */
+    public static function csvToArray($filePath)
+    {
+        // TODO: CSVファイルかどうか
+
+        $file = new \SplFileObject($filePath);
+        $file->setFlags(\SplFileObject::READ_CSV);
+
+        $array = [];
+        foreach ($file as $line) {
+            mb_convert_variables('UTF-8', ['ASCII', 'JIS', 'EUC-JP', 'SJIS'], $line[0]);
+            $array[] = $line;
+        }
+
+        return $array;
+    }
+
+    /**
+     * 文字列を配列に変換する
+     *
+     * @param $string
+     * @return array
+     */
+    public static function stringToArray($string)
+    {
+        $string = str_replace(' ', '', $string);
+        $array = explode(',', $string);
+        return $array;
+    }
 }
