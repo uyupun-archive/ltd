0022705f8afffdb7d137813130da00c47ba4de9a
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-27 18:06:12 +0900

    Modify LTTimer

--- app/LTTimer.php
+++ app/LTTimer.php
@@ -25,8 +25,9 @@ class LTTimer
     public function countdown()
     {
         $second = $this->minutes * 60;
+        $pad = "     \x08\x08\x08\x08\x08";
         for ($i = $second; $i >= 0; --$i) {
-            echo "\r\033[0;32m" . $i . " seconds left     " . "\033[0m";
+            echo "\r\033[0;32m" . $i . ' seconds left' . "\033[0m" . $pad;
             sleep(1);
         }
         echo "\r\033[0;32m" . '----- KANKANKANKANKANKAN!!!!!!!!!!! -----' . "\033[0m" . PHP_EOL;
